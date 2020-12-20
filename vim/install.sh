f7f09f6cab4400be851d17b61cc9c5d53f20febf
Dane Mortensen <mortensendane@gmail.com>
2020-12-20 00:53:25 -0800

    Update install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,1 +1,15 @@
-ln -s vimrc ~/.vimrc
+wd=$(pwd)
+
+echo "linking $wd/vim to ~/.vim"
+ln -s $wd/vim ~/.vim
+echo "linking $wd/vimrc to ~/.vimrc"
+ln -s $wd/vimrc ~/.vimrc
+
+echo "downloading plug.vim to ~/.vim/autoload"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "installing plugins"
+vim -c ":PlugInstall"
+
+echo "vim installation complete"
