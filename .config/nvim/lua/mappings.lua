22c39c7f49adaf1ba041af65d40c3ca4629ccf0d
Dane Mortensen <mortensendane@gmail.com>
2021-12-19 16:42:15 -0800

    Remove use of utils from mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,19 +1,19 @@
 local M = {}
 
-local map = require('utils').map
+local map = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
 
 cmd = vim.cmd
 
 vim.g.mapleader = ' '      -- Map leader to space
 vim.g.maplocalleader = ',' -- Map local leader to comma
 
-
-map('i', 'jj', '<Esc>') -- jj to escape in insert mode
+map('i', 'jj', '<Esc>', opts)  -- 'jj' to enter normal mode from insert mode
 
 cmd 'ca tn tabnew'
 
 M.nvimTree = function()
-  map('n', '<C-n>', ':NvimTreeToggle <CR>')
+  map('n', '<C-n>', ':NvimTreeToggle <CR>', opts)
 end
 
 return M
