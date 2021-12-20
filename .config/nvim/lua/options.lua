dca99311d1d8a029cc76317378e94fac0519b91f
Dane Mortensen <mortensendane@gmail.com>
2021-12-19 16:28:35 -0800

    Overhaul neovim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,7 +1,15 @@
-local utils = require('utils')
+local options = {
+  clipboard = 'unnamedplus',  -- Always use the system clipboard
+  expandtab = true,           -- Convert tabs to spaces
+  hlsearch = true,            -- Highlight all matches of search pattern
+  ignorecase = true,          -- Ignore case in searches
+  number = true,              -- Display line numbers
+  relativenumber = true,      -- Use relative line numbers
+  shiftwidth = 4,             -- Use four spaces per (auto)indent
+  tabstop = 4,                -- Display each <Tab> as four spaces
+  wrap = false,               -- Display long lines as single lines
+}
 
--- LINE NUMBERS
-utils.opt('w', 'number', true)
-utils.opt('w', 'relativenumber', true)
-
-utils.opt('o', 'gdefault', true) -- Always perform global substitutions
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
