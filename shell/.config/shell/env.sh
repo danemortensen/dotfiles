160caf5831f9e31dcf40a192a683e8ff8668c127
Dane Mortensen <mortensendane@gmail.com>
2025-06-28 11:19:04 -0700

    Set default editor to vim

--- shell/.config/shell/env.sh
+++ shell/.config/shell/env.sh
@@ -1,26 +1,37 @@
 # This file defines common environment variables to be shared between different
-# shells (e.g. bash and zsh). Note that XDG base directories should already be
-# set when this file is executed.
+# shells (e.g. bash and zsh).
 
 # ------------------------------------------------------------------------------
-# Set XDG base directories, if they haven't already been set.
+# XDG base directories
 #
 # These environment variables define standard locations for user-specific files
-# and help keep your home directory clean and organized.
+# and help keep your home directory clean and organized. I suggest setting these
+# before this file is executed, but this provides some sensible defaults in case
+# those variables have not been set already.
 #
-# - XDG_CONFIG_HOME: User-specific configuration files.
-# - XDG_CACHE_HOME: Non-essential cached data (e.g. pip wheels, web caches).
-# - XDG_DATA_HOME: Persistent user data (e.g. themes, icons, Rust packages).
-# - XDG_STATE_HOME: Stateful data (e.g. logs, history files, or lock files).
+# - XDG_CONFIG_HOME: User-specific configuration files
+# - XDG_CACHE_HOME: Non-essential cached data (e.g. pip wheels, web caches)
+# - XDG_DATA_HOME: Persistent user data (e.g. themes, icons, Rust packages)
+# - XDG_STATE_HOME: Stateful data (e.g. logs, history files, or lock files)
 # ------------------------------------------------------------------------------
 : "${XDG_CONFIG_HOME:=$HOME/.config}"
 : "${XDG_CACHE_HOME:=$HOME/.cache}"
 : "${XDG_DATA_HOME:=$HOME/.local/share}"
 : "${XDG_STATE_HOME:=$HOME/.local/state}"
 
-# EDITOR
-export EDITOR="nvim"
-export VISUAL="nvim"
+# Set default editors
+# - VISUAL: Full-screen, interactive editor (e.g. crontabl -e, git)
+# - EDITOR: Basic editor or fallback (e.g. sudoedit, mail)
+if command -v vim >/dev/null 2>&1; then
+  export VISUAL=vim
+  export EDITOR=vim
+elif command -v nano >/dev/null 2>&1; then
+  export VISUAL=nano
+  export EDITOR=nano
+else
+  export VISUAL=vi
+  export EDITOR=vi
+fi
 
 # Less
 export LESSHISTFILE="$XDG_STATE_HOME/less/history"  # Set history location
@@ -56,3 +67,4 @@ fi
 if [ -f "$CARGO_HOME/env" ]; then
     . "$CARGO_HOME/env"
 fi
+
