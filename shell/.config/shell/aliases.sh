3a44a2b05e10fb3cb992a12d0b0275553f2d62a9
Dane Mortensen <mortensendane@gmail.com>
2025-07-02 22:56:11 -0700

    Add precedence of nvim, vim, vi to 'v' alias

--- shell/.config/shell/aliases.sh
+++ shell/.config/shell/aliases.sh
@@ -18,6 +18,18 @@ alias la='ls -A'
 alias ll='ls -lAhF'
 
 alias tree='tree -AC'
-alias v='nvim'
+
+v() {
+  if command -v nvim &> /dev/null; then   # 1. Try nvim
+    nvim "$@"
+  elif command -v vim &> /dev/null; then  # 2. Try vim
+    vim "$@"
+  elif command -v vi &> /dev/null; then   # 3. Try vi
+    vi "$@"
+  else                                    # 4. Error out
+    echo "Error: No editor (nvim, vim, or vi) found in your PATH." >&2
+    return 1
+  fi
+}
 
 alias sudo='sudo ' # Allow sudo to work with aliases
