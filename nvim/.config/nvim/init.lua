62da04308b6bf885707fc0aa0faf21e91bc7eeee
Dane Mortensen <mortensendane@gmail.com>
2026-08-22 14:01:22 -0700

    Add visual options

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,12 @@
+-- Visual & UI Layout
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.syntax = "enable"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
 
 -- Clears the cache and reloads the neovim config.
