fe29a9224da1eb0d5b404264869a38bfc89a3e8d
Dane Mortensen <mortensendane@gmail.com>
2026-08-22 13:52:07 -0700

    Add keymap to reload the nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,19 @@
 vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode" })
+
+-- Clears the cache and reloads the neovim config.
+local function reload_config()
+    -- Clear cached modules belonging to the config.
+    for name, _ in pairs(package.loaded) do
+        if name:match("^user") or name:match("^init") then
+            package.loaded[name] = nil
+        end
+    end
+
+    -- Reload the main init.lua file.
+    dofile(vim.env.MYVIMRC)
+    vim.notify("Config reloaded!", vim.log.levels.INFO)
+end
+
+-- Reload the config with <leader>rs.
+vim.keymap.set("n", "<leader>rs", reload_config, { desc = "Reload Neovim config" })
+
