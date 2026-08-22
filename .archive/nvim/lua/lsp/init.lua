44128db2a862756cacffc72deb1344635ba1124c
Dane Mortensen <mortensendane@gmail.com>
2026-08-22 13:38:19 -0700

    Archive old neovim config

--- .archive/nvim/lua/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local installed, _ = pcall(require, 'lspconfig')
-if not installed then
-  print('Unable to set up LSP support. Please install lspconfig first.')
-  return
-end
-
-require('lsp.lsp-installer')
-require('lsp.handlers').setup()
